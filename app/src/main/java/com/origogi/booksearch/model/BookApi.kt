715a019e702eeaf819c202886d2a33b3fdb94869
origogi <rlawjdxo1212@naver.com>
2021-12-01 22:06:53 +0900

    (feat) Implement `getBooks` api
    - Implement Retrofit service
    - Add Book model class
    - Add INTERNET permission

---
+++ app/src/main/java/com/origogi/booksearch/model/BookApi.kt
@@ -0,0 +1,49 @@
+package com.origogi.booksearch.model
+
+import com.origogi.booksearch.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface BookApi {
+    @GET("{keyword}/{page}")
+    suspend fun getBooks(
+        @Path("keyword") keyword: String,
+        @Path("page") page: Int
+    ): Response
+}
+
+data class Response (
+    var error : Int =0,
+    var total : Int = 0,
+    var page: Int = 0,
+    var books : List<Book> = emptyList()
+)
+
+
+object RetrofitService {
+
+    val bookApi: BookApi by lazy {
+        val client = OkHttpClient.Builder().apply {
+            if (BuildConfig.DEBUG) {
+                val logger = HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+                addInterceptor(logger)
+            }
+        }.build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.itbook.store/1.0/search/")
+            .client(
+                client
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(BookApi::class.java)
+    }
+}
--- app/src/main/java/com/origogi/booksearch/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/origogi/booksearch/viewmodel/MainViewModel.kt
@@ -2,11 +2,25 @@ package com.origogi.booksearch.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.origogi.booksearch.TAG
+import com.origogi.booksearch.model.RetrofitService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
 
-    fun search(words : String) {
+    fun search(words: String) {
         Log.d(TAG, "search $words")
+
+        viewModelScope.launch {
+            val response =
+                RetrofitService.bookApi.getBooks(words, 1)
+
+            Log.d(TAG, "result $response")
+
+        }
+
     }
 }
