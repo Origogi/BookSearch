bece19e3c63928f897d8ae56bc8bfbb010b08335
origogi <rlawjdxo1212@naver.com>
2021-12-02 11:33:37 +0900

    (feat) Implement PageIndexer
    - Add `PageIndexer` class in ViewModel
    - Add logic that check current page and update page count

--- app/src/main/java/com/origogi/booksearch/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/origogi/booksearch/viewmodel/MainViewModel.kt
@@ -13,37 +13,86 @@ import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
+    private var includeWords: List<String> = emptyList()
+    private var excludeWord = ""
+    private val pageIndexerMap = mutableMapOf<String, PageIndexer>()
+
     private val _books: MutableLiveData<List<Book>> = MutableLiveData()
     val books: LiveData<List<Book>>
         get() = _books
 
-    fun search(words: List<String>, excludeWord: String) {
-        Log.d(TAG, "search $words $excludeWord")
-
+    fun search(words: List<String>, excWord: String) {
+        Log.d(TAG, "search $words $excWord")
         viewModelScope.launch {
-            _books.value = emptyList()
+            pageIndexerMap.clear()
+            includeWords = words
 
-            words.map {
-                async {
-                    RetrofitService.bookApi.getBooks(it, 1)
-                }
-            }.map {
-                it.await()
-            }.map {
-                it.books
+            includeWords.forEach {
+                pageIndexerMap[it] = PageIndexer()
             }
-            .map { books ->
-                if (excludeWord.isNotBlank()) {
-                    books.filter {
-                        !it.title.lowercase().contains(excludeWord.lowercase())
+
+            excludeWord = excWord
+            _books.value = emptyList()
+            fetch()
+        }
+    }
+
+    fun loadMore() {
+        fetch()
+    }
+
+    private fun fetch() {
+        viewModelScope.launch {
+
+            includeWords
+                .filter { word ->
+                    pageIndexerMap[word]!!.hasNextPage()
+                }
+                .map { word ->
+                    async {
+                        RetrofitService.bookApi.getBooks(word, pageIndexerMap[word]!!.getNextPage())
                     }
-                } else {
-                    books
+                }.map {
+                    it.await()
+                }.mapIndexed { i, response ->
+
+                    pageIndexerMap[includeWords[i]]!!.updateCount(
+                        response.books.size,
+                        response.total
+                    )
+
+                    response.books
                 }
-            }.forEach { books ->
-                _books.value = (_books.value ?: emptyList()) + books
-            }
+                .map { books ->
+                    if (excludeWord.isNotBlank()) {
+                        books.filter {
+                            !it.title.lowercase().contains(excludeWord.lowercase())
+                        }
+                    } else {
+                        books
+                    }
+                }.forEach { books ->
+                    _books.value = (_books.value ?: emptyList()) + books
+                }
+        }
+    }
+
+    private class PageIndexer {
+        private var currentCount = 0
+        private var maxCount = 0
+        private var currentPage = 1
+
+        fun hasNextPage(): Boolean {
+            return (currentCount < maxCount) || (currentCount == 0)
         }
 
+        fun getNextPage(): Int {
+            return currentPage++
+        }
+
+        fun updateCount(responseCurrentCount: Int, responseMaxCount: Int) {
+            currentCount += responseCurrentCount
+            maxCount = responseMaxCount
+        }
     }
 }
