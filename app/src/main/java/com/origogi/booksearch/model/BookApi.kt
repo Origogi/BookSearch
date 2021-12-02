91df9acc294bcffc09f1938b60f1b7b087a5cad3
origogi <rlawjdxo1212@naver.com>
2021-12-02 15:58:11 +0900

    (feat) Implement BookDetail api

--- app/src/main/java/com/origogi/booksearch/model/BookApi.kt
+++ app/src/main/java/com/origogi/booksearch/model/BookApi.kt
@@ -7,21 +7,43 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface BookApi {
-    @GET("{keyword}/{page}")
+    @GET("search/{keyword}/{page}")
     suspend fun getBooks(
         @Path("keyword") keyword: String,
         @Path("page") page: Int
-    ): Response
+    ): BookListResponse
+
+    @GET("books/{isbn}")
+    suspend fun getBook(
+        @Path("isbn") isbn: String,
+    ): BookResponse
 }
 
-data class Response (
+data class BookListResponse (
     var error : Int =0,
     var total : Int = 0,
     var page: Int = 0,
     var books : List<Book> = emptyList()
+)
+
+data class BookResponse (
+    var error : Int =0,
+    val title: String,
+    var subtitle: String = "",
+    var authors: String = "",
+    var publisher: String = "",
+    var language: String = "",
+    var isbn10: String = "",
+    var isbn13: String = "",
+    var pages: String = "",
+    var year: String = "",
+    var rating: String = "",
+    var desc: String = "",
+    var price: String = "",
+    var image: String = "",
+    var url: String = "",
 )
 
 
@@ -38,7 +60,7 @@ object RetrofitService {
         }.build()
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.itbook.store/1.0/search/")
+            .baseUrl("https://api.itbook.store/1.0/")
             .client(
                 client
             )
--- app/src/main/java/com/origogi/booksearch/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/origogi/booksearch/viewmodel/DetailViewModel.kt
@@ -1,12 +1,44 @@
 package com.origogi.booksearch.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.origogi.booksearch.TAG
+import com.origogi.booksearch.model.BookDetail
+import com.origogi.booksearch.model.RetrofitService
+import kotlinx.coroutines.launch
 
 class DetailViewModel : ViewModel() {
 
+    private val _bookDetail = MutableLiveData<BookDetail>()
+    val bookDetail : LiveData<BookDetail>
+        get() = _bookDetail
+
     fun loadDataFromISBN(isbn : String) {
         Log.d(TAG, "isbn : $isbn")
+
+        viewModelScope.launch {
+            val response = RetrofitService.bookApi.getBook(isbn)
+            Log.d(TAG, "response : $response")
+
+            _bookDetail.value = BookDetail(
+                response.title,
+                response.subtitle,
+                response.authors,
+                response.publisher,
+                response.language,
+                response.isbn10,
+                response.isbn13,
+                response.pages,
+                response.year,
+                response.rating,
+                response.desc,
+                response.price,
+                response.image,
+                response.url
+            )
+        }
     }
 }
