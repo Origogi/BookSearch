ff11c36e9f9d4c76328663c4708f1a27cdcebb23
origogi <rlawjdxo1212@naver.com>
2021-12-02 17:17:55 +0900

    (feat) Add DetailPage Compose
    - Add Appbar
    - Add Title
    - Add Image

--- app/src/main/java/com/origogi/booksearch/Constants.kt
+++ app/src/main/java/com/origogi/booksearch/Constants.kt
@@ -1,7 +1,26 @@
 package com.origogi.booksearch
 
+import com.origogi.booksearch.model.BookDetail
+
 const val TAG = "KJT"
 
 enum class State {
     IDLE, LOADING
 }
+
+val dummyData = BookDetail(
+    "Learning Android Application Programming for the Kindle Fire",
+    "A Hands-On Guide to Building Your First Android Application",
+    "Lauren Darcey, Shane Conder",
+    "Addison-Wesley",
+    "English",
+    "032183397X",
+    "9780321833976",
+    "352",
+    "2012",
+    "4",
+    "In this book, bestselling Android programming authors Lauren Darcey and Shane Conder teach you every skill and technique you need to write production-quality apps for Amazon Kindle Fire, the world's hottest Android tablet. You'll learn the very best way: by building a complete app from start to fini...",
+    "$11.96",
+    "https://itbook.store/img/books/9780321833976.png",
+    "https://itbook.store/books/9780321833976"
+)
--- app/src/main/java/com/origogi/booksearch/view/DetailActivity.kt
+++ app/src/main/java/com/origogi/booksearch/view/DetailActivity.kt
@@ -1,35 +1,119 @@
 package com.origogi.booksearch.view
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.databinding.DataBindingUtil
+import coil.compose.rememberImagePainter
 import com.origogi.booksearch.R
 import com.origogi.booksearch.TAG
 import com.origogi.booksearch.databinding.ActivityDetailBinding
 import com.origogi.booksearch.databinding.ActivityMainBinding
+import com.origogi.booksearch.dummyData
+import com.origogi.booksearch.model.BookDetail
 import com.origogi.booksearch.viewmodel.DetailViewModel
 import com.origogi.booksearch.viewmodel.MainViewModel
+import java.util.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private val viewModel: DetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportActionBar?.hide()
+
+        viewModel.loadDataFromISBN(intent.getStringExtra("isbn")!!)
+
         setContent {
-            DetailScreen()
+            DetailScreen(viewModel)
         }
     }
 }
 
 @Composable
-fun DetailScreen() {
-    MaterialTheme {
-        Text(text = "compose screen")
+fun DetailScreen(viewModel: DetailViewModel) {
+    val bookDetail = viewModel.bookDetail.observeAsState().value
+
+    if (bookDetail != null) {
+        DetailPage(bookDetail)
     }
+
+}
+
+@Composable
+fun DetailPage(bookDetail: BookDetail) {
+    MaterialTheme {
+        Scaffold(
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Appbar()
+                Text(bookDetail.title, textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(horizontal = 16.dp))
+                Image(
+                    painter = rememberImagePainter(bookDetail.image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(200.dp)
+                        .aspectRatio(300.0f / 350.0f)
+                        .clip(
+                            RoundedCornerShape(10.dp)
+                        )
+                )
+            }
+
+        }
+    }
+}
+
+
+@Composable
+fun Appbar() {
+    val activity = (LocalContext.current as? Activity)
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        IconButton(onClick = { activity?.finish() }) {
+            Icon(Icons.Default.ArrowBack, contentDescription = "")
+        }
+
+    }
+}
+
+
+@Composable
+@Preview
+fun DetailScreenPreview() {
+
+    DetailPage(dummyData)
 }
