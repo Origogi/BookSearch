7c035da1b3a90f0c359a92a08a9954c161ba5624
origogi <rlawjdxo1212@naver.com>
2021-12-02 16:01:28 +0900

    (build) Add compose library

--- app/src/main/java/com/origogi/booksearch/view/DetailActivity.kt
+++ app/src/main/java/com/origogi/booksearch/view/DetailActivity.kt
@@ -3,7 +3,11 @@ package com.origogi.booksearch.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.databinding.DataBindingUtil
 import com.origogi.booksearch.R
 import com.origogi.booksearch.TAG
@@ -13,19 +17,19 @@ import com.origogi.booksearch.viewmodel.DetailViewModel
 import com.origogi.booksearch.viewmodel.MainViewModel
 
 class DetailActivity : AppCompatActivity() {
-    private val viewModel : DetailViewModel by viewModels()
-    private lateinit var binding : ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
 
-        val isbn = intent.getStringExtra("isbn") ?: ""
+        setContent {
+            DetailScreen()
+        }
+    }
+}
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
-        binding.lifecycleOwner = this
-        binding.viewmodel = viewModel
-
-        viewModel.loadDataFromISBN(isbn)
-
+@Composable
+fun DetailScreen() {
+    MaterialTheme {
+        Text(text = "compose screen")
     }
 }
