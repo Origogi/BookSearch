1eb4f104b4d606c9e7b12c83a649a2d75bbbd6ff
origogi <rlawjdxo1212@naver.com>
2021-12-03 11:13:35 +0900

    (res) Add poppins font family

---
+++ app/src/main/java/com/origogi/booksearch/view/compose/Typography.kt
@@ -0,0 +1,68 @@
+package com.origogi.booksearch.view.compose
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.origogi.booksearch.R
+
+
+private val Poppins = FontFamily(
+    Font(R.font.poppins),
+    Font(R.font.poppins_medium, FontWeight.W500),
+    Font(R.font.poppins_semibold, FontWeight.W600)
+)
+
+
+val Typography = Typography(
+    h4 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W600,
+        fontSize = 30.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W600,
+        fontSize = 16.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Poppins,
+        fontSize = 14.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = Poppins,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp
+    )
+)
